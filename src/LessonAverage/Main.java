e6b9f261b1921f3773456ec1bf8dc154c61b6caa
XerneaX <gakbal112@gmail.com>
2022-02-03 03:43:29 +0300

    File names changed

--- src/LessonAverage/Main.java
+++ src/LessonAverage/Main.java
@@ -1,18 +1,9 @@
-package DersOrtalaması;
+package LessonAverage;
 
 import java.util.Scanner;
 
-public class LessonAverage {
+public class Main {
 
-    /*
-    * Not Ortalaması Hesaplayan Program
-Java ile Matematik, Fizik, Kimya, Türkçe, Tarih, Müzik derslerinin sınav puanlarını kullanıcıdan alan
-*  ve ortalamalarını hesaplayıp ekrana bastırılan programı yazın.
-*
-
-Ödev
-Aynı program içerisinde koşullu ifadeler kullanılarak, eğer kullanıcının ortalaması 60'dan büyük ise ekrana "Sınıfı Geçti" ,
-* küçük ise "Sınıfta Kaldı" yazsın.*/
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int math,fzk, kimya, tr, tarih,music;
