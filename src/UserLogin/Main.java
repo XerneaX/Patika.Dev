dafc8169afa809c42c9cd54058b8e170b12e2341
XerneaX <gakbal112@gmail.com>
2022-02-23 19:58:39 +0300

    UserLogin updated

--- src/UserLogin/Main.java
+++ src/UserLogin/Main.java
@@ -4,13 +4,6 @@ package UserLogin;
 import java.util.Scanner;
 
 public class Main {
-    /*
-    Eğer şifre yanlış ise kullanıcıya şifresini sıfırlayıp sıfırlamayacağını sorun,
-    eğer kullanıcı sıfırlamak isterse yeni girdiği şifrenin hatalı girdiği ve
-    unuttuğu şifre ile aynı olmaması gerektiğini kontrol edip , şifreler aynı ise ekrana
-    "Şifre oluşturulamadı, lütfen başka şifre giriniz." sorun yoksa "Şifre oluşturuldu" yazan programı yazınız.
-    */
-
     public static void main(String[] args){
         String username, password;
         Scanner scanner = new Scanner(System.in);
